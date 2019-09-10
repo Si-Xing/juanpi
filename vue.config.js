eaf1392b98aaefac5efb7c0981521012b986cd98
Si-Xing <18736780230@163.com>
2019-09-10 11:05:26 +0800

    说明

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,24 @@
-const path=require("path");
-
-module.exports={
-    devServer:{
-        proxy:{
-            "/api":{
-                target:"https://webservice.juanpi.com/",
-                changeOrigin:true
+const path = require("path")
+module.exports = {
+    devServer: {
+        proxy: {
+            "/api": {
+                target: "https://webservice.juanpi.com",
+                changeOrigin: true
             }
         }
     },
-    configureWebpack:{
-        resolve:{
-            alias:{
-                "@":path.join(__dirname,"./src"),
-                "api":path.join(__dirname,"./src/api"),
-                "assets":path.join(__dirname,"./src/assets"),
-                "components":path.join(__dirname,"./src/components"),
-                "router":path.join(__dirname,"./src/router"),
-                "store":path.join(__dirname,"./src/store"),
-                "utils":path.join(__dirname,"./src/utils"),
-                "lib":path.join(__dirname,"./src/lib")
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "@": path.join(__dirname, "./src"),
+                "lib":path.join(__dirname,"./src/lib"),
+                "api": path.join(__dirname, "./src/api"),
+                "assets": path.join(__dirname, "./src/assets"),
+                "components": path.join(__dirname, "./src/components"),
+                "router": path.join(__dirname, "./src/router"),
+                "store": path.join(__dirname, "./src/store"),
+                "utils": path.join(__dirname, "./src/utils")
             }
         }
     }
