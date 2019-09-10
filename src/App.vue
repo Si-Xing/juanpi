815f9cfb73e5b4ac1aa65d3fb19503bef8bec9a8
Si-Xing <53642063+Si-Xing@users.noreply.github.com>
2019-09-10 08:50:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <router-view></router-view>
     <Wxs-TabBar/>
   </div>
@@ -11,16 +10,7 @@ import TabBar from "./components/tabBar"
 export default {
   components:{
     [TabBar.name]:TabBar,
-  },
-=======
-   
-    <router-view/>
-  </div>
-</template>
-<script>
-export default {
-  name:"App"
->>>>>>> 0acee20ce6ed6827caf1b3c755e8a070593f48ac
+  }
 }
 </script>
 <style lang="scss">
