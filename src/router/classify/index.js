9b6fe4a974cd85fc389e03a1480c79a7258ad6c7
Si-Xing <53642063+Si-Xing@users.noreply.github.com>
2019-09-10 08:43:01 +0800

    Update index.js

--- src/router/classify/index.js
+++ src/router/classify/index.js
@@ -1,19 +1,8 @@
 export default {
-<<<<<<< HEAD
-    
     path:"/classify",
     name:"全部分类",
     //按需加载引入 ===》路由的懒加载
     component:()=>import("@/pages/classify")
 
 };
-=======
-    path:"/classify",
-    component:()=>import("views/classify"),
-    name:"classify",
-    meta:{
-        tabBar:true,
-        auth:false
-    }
-}
->>>>>>> 0acee20ce6ed6827caf1b3c755e8a070593f48ac
+
