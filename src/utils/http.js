a3e4baed2eaca9507247067155e2cb7eba136f63
Si-Xing <53642063+Si-Xing@users.noreply.github.com>
2019-09-10 08:47:56 +0800

    Update http.js

--- src/utils/http.js
+++ src/utils/http.js
@@ -25,32 +25,3 @@ http.interceptors.response.use(res=>{
 })
 //响应
 export default http
-=======
-import loading from "lib/loadimg/index.js"
-const server = axios.create({
-    timeout:10000,
-    withCredentials:true
-})
-server.interceptors.request.use(config=>{
-    if(config.method == "get"){
-        config.params = {...config.data};
-    }else if(config.method == "POST"){
-        //config.headers["content-type"] = "application/x-www-form-urlencoded"
-    }
-    loading.open()
-    return config;
-},err=>{
-    return Promise.reject(err);
-})
-
-server.interceptors.response.use(res=>{
-    if(res.status == 200){
-        loading.close()
-        return res.data;
-    }
-})
-
-export default server;
-
-
->>>>>>> 0acee20ce6ed6827caf1b3c755e8a070593f48ac
